40dd787e7e6786a51ec2fd2cd6a1d1f6ba4d8b85
Matheus Romano <matheus.romano@integra.ag>
2017-11-12 14:52:12 -0200

    adicionado context lanche pedido

--- 153264_152728/153264_152728/Models/LanchePedidoContext.cs
+++ 153264_152728/153264_152728/Models/LanchePedidoContext.cs
@@ -8,9 +8,10 @@ namespace _153264_152728.Models
 {
     public class LanchePedidoContext : DbContext
     {
-        public LanchePedidoContext() : base("DbLanchonete") 
-         {
-         }
+        public LanchePedidoContext() : base("DbLanchonete")
+        {
+
+        }
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<Comidas> Lanches { get; set; }
 
